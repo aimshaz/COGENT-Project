d74efec17d9a8b956ece09257b4cb12e5180dad9
alferrei <alexandra.ferreira@ugent.be>
2022-06-01 20:50:52 +0200

    added date slider, not showing on screen yet

--- src/charts.py
+++ src/charts.py
@@ -6,31 +6,59 @@ from dash import dcc
 import plotly.graph_objects as go
 import json
 
+import datetime as dt
+import panel as pn
+
+pn.extension()
 
 df_museumLocations = pd.read_csv('../locaties-musea-gent.csv', sep=";")
 df_events = pd.read_csv('../toeristische-evenementen-visit-gent.csv', sep=";")
 
 columns = ['ID', 'Name_NL', 'Name_EN', 'Type', 'Lat', 'Lon', 'Date_Start', 'Date_End', 'Link']
 data = []
+all_start_dates = []
+all_end_dates = []
     
 def collectDataEvents(row):
     coords = row['location'].split()
     lat = coords[2]
     lon = coords[1][1:]
-    data.append([row['identifier'], row['name_nl'], row['name_en'], 'EVENT', lat, lon, row['date_start'], row['date_end'], row['_event']])
+    all_start_dates.append(dt.datetime(int(row['date_start'][0:4]), int(row['date_start'][5:7]), int(row['date_start'][8:10])))
+    all_end_dates.append(dt.datetime(int(row['date_end'][0:4]), int(row['date_end'][5:7]), int(row['date_end'][8:10])))
+    data.append([row['identifier'], row['name_nl'], row['name_en'], 'EVENT', lat, lon, all_start_dates[len(all_start_dates)-1], all_end_dates[len(all_end_dates)-1], row['_event']])
 df_events.apply(collectDataEvents, axis=1)
 
 df_eventParsed = pd.DataFrame(data, columns = columns)
-
+all_start_dates.sort()
+all_end_dates.sort()
 
 def getCoords(row):
     return [row['longitude'], row['latitude']]
 
+def filterData(new_start_date, new_end_date):
+    filteredDf = df_eventParsed.drop(df_eventParsed[df_eventParsed.Date_Start <= new_start_date].index)
+    filteredDf = filteredDf.drop(filteredDf[filteredDf.Date_End >= new_end_date].index)
+    return filteredDf
+
 def map():
     #q1
+
+    date_range_slider = pn.widgets.DateRangeSlider(
+        name='Date Range Slider',
+        start=all_start_dates[0], end=all_end_dates[len(all_end_dates)-1],
+        value=(all_start_dates[0], all_end_dates[len(all_end_dates)-1]),
+    )
+
+    date_range_slider
+
+    new_start_date = date_range_slider.value[0]
+    new_end_date = date_range_slider.value[1]
+
+    filteredDf = filterData(new_start_date, new_end_date)
+    
     gent_map = json.load(open("../locaties-musea-gent.geojson"))
 
-    fig = px.choropleth_mapbox(df_eventParsed, geojson=gent_map, 
+    fig = px.choropleth_mapbox(filteredDf, geojson=gent_map, 
                            mapbox_style="carto-positron",
                            zoom=12, center = {"lat": 51.049999, "lon": 3.733333},
                            opacity=0.5,
@@ -38,11 +66,11 @@ def map():
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
     #q2
-    fig.add_trace(go.Scattermapbox(lat=df_eventParsed['Lat'], lon=df_eventParsed['Lon'], name="event information", mode = 'markers', hoverinfo = 'text', marker=dict(color='LightSkyBlue'),
-        hovertext = '<b>Name</b>: '+ df_eventParsed['Name_EN'].astype(str) +'<br>'
-                    '<b>Start Date</b>: '+ df_eventParsed['Date_Start'].astype(str) +'<br>'
-                    '<b>End Date</b>: '+ df_eventParsed['Date_End'].astype(str) +'<br>'
-                    '<b>Link</b>: '+ df_eventParsed['Link'].astype(str) +'<br>'
+    fig.add_trace(go.Scattermapbox(lat=filteredDf['Lat'], lon=filteredDf['Lon'], name="event information", mode = 'markers', hoverinfo = 'text', marker=dict(color='LightSkyBlue'),
+        hovertext = '<b>Name</b>: '+ filteredDf['Name_EN'].astype(str) +'<br>'
+                    '<b>Start Date</b>: '+ filteredDf['Date_Start'].astype(str) +'<br>'
+                    '<b>End Date</b>: '+ filteredDf['Date_End'].astype(str) +'<br>'
+                    '<b>Link</b>: '+ filteredDf['Link'].astype(str) +'<br>'
 
         ,showlegend=True))
 
